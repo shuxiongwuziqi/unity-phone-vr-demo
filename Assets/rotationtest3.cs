7a644e43f57e01fe3746a94f5fa6fb787795bf8e
wuziqi <1306557901@qq.com>
2019-10-09 20:30:44 +0800

    修改

---
+++ Assets/rotationtest3.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rotationtest3 : MonoBehaviour
+{
+    private bool gyroBool;
+    private Gyroscope gyro;
+    private Quaternion rotFix;
+    private Vector3 initial = new Vector3(90, 180, 0);
+
+    // Use this for initialization
+    void Start()
+    {
+        Screen.orientation = ScreenOrientation.LandscapeLeft;
+        Screen.sleepTimeout = SleepTimeout.NeverSleep;
+
+        gyroBool = SystemInfo.supportsGyroscope;
+
+        Debug.Log("gyro bool = " + gyroBool.ToString());
+
+        if (gyroBool)
+        {
+            gyro = Input.gyro;
+            gyro.enabled = true;
+
+            rotFix = new Quaternion(0, 0, 0.7071f, 0.7071f);
+        }
+        else
+        {
+            Debug.Log("No Gyro Support");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (gyroBool)
+        {
+            var camRot = gyro.attitude * rotFix;
+            transform.eulerAngles = initial;
+            transform.localRotation *= camRot;
+        }
+    }
+}
